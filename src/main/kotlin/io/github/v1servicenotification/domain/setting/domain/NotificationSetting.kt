86006cc58b6a71b64562050eb338ec4a57da381f
Lee SeoJune <sung07288346@gmail.com>
2022-03-17 19:57:40 +0900

    📑 :: NotificationSetting entity

---
+++ src/main/kotlin/io/github/v1servicenotification/domain/setting/domain/NotificationSetting.kt
@@ -0,0 +1,19 @@
+package io.github.v1servicenotification.domain.setting.domain
+
+import javax.persistence.Column
+import javax.persistence.EmbeddedId
+import javax.persistence.Entity
+import javax.persistence.Table
+import javax.validation.constraints.NotNull
+
+
+@Table(name = "tbl_notification_setting")
+@Entity
+class NotificationSetting(
+        @EmbeddedId
+        val settingId: SettingId,
+
+        @field:NotNull
+        @Column(columnDefinition = "TINYINT(1) DEFAULT 0")
+        val isActivated: Boolean
+)
