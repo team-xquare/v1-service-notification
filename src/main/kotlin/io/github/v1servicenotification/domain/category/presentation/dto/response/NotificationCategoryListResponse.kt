23ffd09821c1632f40639306617f242d68aa465b
Lee SeoJune <sung07288346@gmail.com>
2022-03-17 22:39:55 +0900

    📑 :: NotificationCategoryListResponse dto

---
+++ src/main/kotlin/io/github/v1servicenotification/domain/category/presentation/dto/response/NotificationCategoryListResponse.kt
@@ -0,0 +1,7 @@
+package io.github.v1servicenotification.domain.category.presentation.dto.response
+
+class NotificationCategoryListResponse(
+        val categories: List<NotificationCategory>
+) {
+
+}
