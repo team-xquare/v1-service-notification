3ad1eff3afdaaf347ffe361b779e8ae967aa99af
Lee SeoJune <sung07288346@gmail.com>
2022-03-22 12:52:36 +0900

    ⚡️ :: id 추가

--- src/main/kotlin/io/github/v1servicenotification/domain/category/presentation/dto/response/NotificationCategory.kt
+++ src/main/kotlin/io/github/v1servicenotification/domain/category/presentation/dto/response/NotificationCategory.kt
@@ -1,6 +1,9 @@
 package io.github.v1servicenotification.domain.category.presentation.dto.response
 
+import java.util.*
+
 class NotificationCategory(
+        val id: UUID,
         val name: String,
         val destination: String
 )
