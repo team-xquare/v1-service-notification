2bd0c9ff2cbd91975481d244fdab899d59c289e2
Lee SeoJune <sung07288346@gmail.com>
2022-03-23 17:12:56 +0900

    📑 :: SecurityConfig

---
+++ src/main/kotlin/io/github/v1servicenotification/global/config/SecurityConfig.kt
@@ -0,0 +1,27 @@
+package io.github.v1servicenotification.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig: WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http
+                .formLogin().disable()
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and().apply(FilterConfig())
+
+    }
+
+}
