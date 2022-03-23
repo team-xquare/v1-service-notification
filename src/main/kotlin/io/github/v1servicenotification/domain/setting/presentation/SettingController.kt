ab18ec912897e1bce2f4a5c574a1f415365187af
Lee SeoJune <sung07288346@gmail.com>
2022-03-23 18:03:16 +0900

    📑 :: SettingController

---
+++ src/main/kotlin/io/github/v1servicenotification/domain/setting/presentation/SettingController.kt
@@ -0,0 +1,29 @@
+package io.github.v1servicenotification.domain.setting.presentation
+
+import io.github.v1servicenotification.domain.setting.service.ActivateNotificationCategoryService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RequestMapping("/tags")
+@RestController
+class SettingController(
+        private val activateNotificationCategoryService: ActivateNotificationCategoryService,
+        private val deActivateNotificationCategoryService: ActivateNotificationCategoryService
+) {
+
+    @PatchMapping("/{category-uuid}")
+    fun activateNotificationCategory(@PathVariable("category-uuid") uuid: UUID): ResponseEntity<Unit> {
+        return ResponseEntity(HttpStatus.valueOf(
+                        activateNotificationCategoryService.execute(uuid)
+                ))
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{category-uuid}")
+    fun deActivateNotificationCategory(@PathVariable("category-uuid") uuid: UUID) {
+        deActivateNotificationCategoryService.execute(uuid)
+    }
+
+}
