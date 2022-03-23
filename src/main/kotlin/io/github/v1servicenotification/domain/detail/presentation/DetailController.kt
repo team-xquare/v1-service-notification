de8937fd63d75f8457ff471014e554bda2057fd2
Lee SeoJune <sung07288346@gmail.com>
2022-03-23 18:35:12 +0900

    📑 :: DetailController

---
+++ src/main/kotlin/io/github/v1servicenotification/domain/detail/presentation/DetailController.kt
@@ -0,0 +1,18 @@
+package io.github.v1servicenotification.domain.detail.presentation
+
+import io.github.v1servicenotification.domain.detail.presentation.dto.response.NotificationDetailListResponse
+import io.github.v1servicenotification.domain.detail.service.QueryNotificationDetailListService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class DetailController(
+        private val queryNotificationDetailListService: QueryNotificationDetailListService
+) {
+
+    @GetMapping("/")
+    fun queryNotificationDetailList(): NotificationDetailListResponse {
+        return queryNotificationDetailListService.execute()
+    }
+
+}
