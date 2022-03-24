fcd1640ce8018e35f760acfc39105f3120efa149
Leeseojune <sung07288346@gmail.com>
2022-03-24 11:48:49 +0900

    ♻️ :: 가독성

--- src/main/kotlin/io/github/v1servicenotification/domain/detail/service/QueryNotificationDetailListService.kt
+++ src/main/kotlin/io/github/v1servicenotification/domain/detail/service/QueryNotificationDetailListService.kt
@@ -1,8 +1,8 @@
 package io.github.v1servicenotification.domain.detail.service
 
 import io.github.v1servicenotification.domain.detail.domain.repository.NotificationDetailRepository
-import io.github.v1servicenotification.domain.detail.presentation.dto.response.NotificationDetailResponse
 import io.github.v1servicenotification.domain.detail.presentation.dto.response.NotificationDetailListResponse
+import io.github.v1servicenotification.domain.detail.presentation.dto.response.NotificationDetailResponse
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -14,11 +14,17 @@ class QueryNotificationDetailListService(
     fun execute(): NotificationDetailListResponse {
         return NotificationDetailListResponse(
                 notificationDetailRepository.findAllByUserId(UUID.randomUUID()) //TODO userId
-                .map {
-                    NotificationDetailResponse(id = it.id, title = it.title, content = it.content, sentAt = it.sentAt,
-                        isRead = it.isRead, userId = it.userId, name = it.getCategoryName(),
-                        destination = it.getCategoryDestination())
-                }.toList()
+                        .map {
+                            NotificationDetailResponse(
+                                    id = it.id,
+                                    title = it.title,
+                                    content = it.content,
+                                    sentAt = it.sentAt,
+                                    isRead = it.isRead,
+                                    userId = it.userId,
+                                    name = it.getCategoryName(),
+                                    destination = it.getCategoryDestination())
+                        }.toList()
         )
     }
 
