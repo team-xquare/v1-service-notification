30deefd285770ed739ea63ea6440a2166e0614ec
sung07288346@gmail.com <>
2022-05-01 01:50:15 +0900

    📑 :: CategoryListResponse

---
+++ notification-domain/src/main/kotlin/io/github/v1servicenotification/category/queryCategory/api/dto/response/CategoryElement.kt
@@ -0,0 +1,9 @@
+package io.github.v1servicenotification.category.queryCategory.api.dto.response
+
+import java.util.*
+
+class CategoryElement(
+    val id: UUID,
+    val name: String,
+    val destination: String
+)
