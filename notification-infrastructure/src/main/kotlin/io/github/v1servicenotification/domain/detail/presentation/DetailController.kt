4aaf8216bf0675596ee1581c4004195f8aca6507
sung07288346@gmail.com <>
2022-05-01 20:47:31 +0900

    ⚡️ :: Controller

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/presentation/DetailController.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/presentation/DetailController.kt
@@ -1,18 +1,19 @@
 package io.github.v1servicenotification.domain.detail.presentation
 
-import io.github.v1servicenotification.domain.detail.presentation.dto.response.NotificationDetailListResponse
-import io.github.v1servicenotification.domain.detail.service.QueryNotificationDetailListService
+import io.github.v1servicenotification.detail.queryDetail.api.QueryNotificationDetail
+import io.github.v1servicenotification.detail.queryDetail.api.dto.response.DetailResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class DetailController(
-    private val queryNotificationDetailListService: QueryNotificationDetailListService
+    private val queryNotificationDetail: QueryNotificationDetail
 ) {
 
     @GetMapping("/")
-    fun queryNotificationDetailList(): NotificationDetailListResponse {
-        return queryNotificationDetailListService.execute()
+    fun queryNotificationDetailList(): DetailResponse {
+        return queryNotificationDetail.queryNotificationDetail(UUID.randomUUID()) //TODO User uuid
     }
 
 }
