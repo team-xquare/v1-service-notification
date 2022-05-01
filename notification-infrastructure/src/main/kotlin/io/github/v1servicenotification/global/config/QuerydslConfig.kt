a1edf0fa8297a5c36661cf7acdc6b56beebe5036
sung07288346@gmail.com <>
2022-05-01 23:45:11 +0900

    📑 :: QuerydslConfig

---
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/global/config/QuerydslConfig.kt
@@ -0,0 +1,16 @@
+package io.github.v1servicenotification.global.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+
+@Configuration
+class QuerydslConfig(
+    private val entityManager: EntityManager
+) {
+
+    @Bean
+    public fun jpaQueryFactory(): JPAQueryFactory = JPAQueryFactory(entityManager)
+
+}
