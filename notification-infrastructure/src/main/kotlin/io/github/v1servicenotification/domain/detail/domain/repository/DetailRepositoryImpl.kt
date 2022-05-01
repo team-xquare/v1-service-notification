3f6bd2ca8ab9c68e8a545c75414d58f66f333d57
sung07288346@gmail.com <>
2022-05-01 23:49:12 +0900

    ⚡️ :: querydsl repo

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/domain/repository/DetailRepositoryImpl.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/domain/repository/DetailRepositoryImpl.kt
@@ -1,17 +1,37 @@
 package io.github.v1servicenotification.domain.detail.domain.repository
 
-import io.github.v1servicenotification.detail.Detail
+import com.querydsl.jpa.impl.JPAQueryFactory
 import io.github.v1servicenotification.detail.queryDetail.spi.DetailRepositorySpi
-import io.github.v1servicenotification.domain.detail.mapper.DetailMapper
+import io.github.v1servicenotification.detail.queryDetail.spi.dto.DetailModel
+import io.github.v1servicenotification.domain.category.domain.QCategoryEntity.categoryEntity
+import io.github.v1servicenotification.domain.detail.domain.QDetailEntity.detailEntity
+import io.github.v1servicenotification.domain.detail.domain.repository.vo.QDetailVO
+import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
 class DetailRepositoryImpl(
-    private val detailRepository: DetailRepository,
-    private val detailMapper: DetailMapper
-): DetailRepositorySpi {
-    override fun findAllByUserId(userId: UUID): List<Detail> {
-        return detailRepository.findAllByUserId(userId)
-            .map { detailMapper.detailEntityToDomain(it) }
+    private val query: JPAQueryFactory,
+) : DetailRepositorySpi {
+    override fun findAllByUserId(userId: UUID): List<DetailModel> {
+        return query
+            .select(
+                QDetailVO(
+                    detailEntity.id,
+                    detailEntity.title,
+                    detailEntity.content,
+                    detailEntity.sentAt,
+                    detailEntity.isRead,
+                    detailEntity.userId,
+                    categoryEntity.name,
+                    categoryEntity.destination
+                )
+            )
+            .join(detailEntity.categoryEntity, categoryEntity)
+            .fetch()
+            .map {
+                DetailModel(it.id, it.title, it.content, it.sentAt, it.isRead, it.userId, it.name, it.destination)
+            }
             .toList()
     }
 }
