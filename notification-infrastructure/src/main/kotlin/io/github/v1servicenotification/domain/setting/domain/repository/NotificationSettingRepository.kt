de8ec3d4c25f395c720907c5dc02e97730c0e0ba
sung07288346@gmail.com <>
2022-05-02 15:50:37 +0900

    📑 :: SettingRepositoryImpl

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/setting/domain/repository/NotificationSettingRepository.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/setting/domain/repository/NotificationSettingRepository.kt
@@ -4,8 +4,10 @@ import io.github.v1servicenotification.domain.setting.domain.SettingEntity
 import io.github.v1servicenotification.domain.setting.domain.SettingId
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface NotificationSettingRepository : CrudRepository<SettingEntity, SettingId> {
-    fun findBySettingId(settingId: SettingId): SettingEntity? //TODO settingId
+    fun findBySettingId(settingId: SettingId): SettingEntity?
+    fun findBySettingIdUserIdAndActivatedIsTrue(userId: UUID): List<SettingEntity>
 }
