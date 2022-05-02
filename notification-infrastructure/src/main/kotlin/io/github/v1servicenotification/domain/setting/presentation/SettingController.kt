aee702a192acc42c5ff5909ef1005b3a71583e4a
sung07288346@gmail.com <>
2022-05-02 15:50:45 +0900

    ⚡️ :: Controller

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/setting/presentation/SettingController.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/setting/presentation/SettingController.kt
@@ -1,6 +1,9 @@
 package io.github.v1servicenotification.domain.setting.presentation
 
-import io.github.v1servicenotification.domain.setting.service.ActivateNotificationCategoryService
+import io.github.v1servicenotification.category.queryCategory.api.dto.response.CategoryListResponse
+import io.github.v1servicenotification.setting.activeSetting.api.ActivateCategory
+import io.github.v1servicenotification.setting.activeSetting.api.DeActivateCategory
+import io.github.v1servicenotification.setting.activeSetting.api.QueryActivatedCategory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -9,23 +12,33 @@ import java.util.*
 @RequestMapping("/tags")
 @RestController
 class SettingController(
-    private val activateNotificationCategoryService: ActivateNotificationCategoryService,
-    private val deActivateNotificationCategoryService: ActivateNotificationCategoryService
+    private val activateCategory: ActivateCategory,
+    private val deActivateCategory: DeActivateCategory,
+    private val queryActivatedCategory: QueryActivatedCategory
 ) {
 
+    @GetMapping
+    fun queryActivatedCategory(): CategoryListResponse {
+        return queryActivatedCategory.queryActivatedCategory(UUID.randomUUID()) //TODO User Id
+    }
+
     @PatchMapping("/{category-uuid}")
-    fun activateNotificationCategory(@PathVariable("category-uuid") uuid: UUID): ResponseEntity<Unit> {
+    fun activateNotificationCategory(@PathVariable("category-uuid") categoryId: UUID): ResponseEntity<Unit> {
         return ResponseEntity(
             HttpStatus.valueOf(
-                activateNotificationCategoryService.execute(uuid)
+                activateCategory.activateCategory(categoryId, UUID.randomUUID()) //TODO User Id
             )
         )
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{category-uuid}")
-    fun deActivateNotificationCategory(@PathVariable("category-uuid") uuid: UUID) {
-        deActivateNotificationCategoryService.execute(uuid)
+    fun deActivateNotificationCategory(@PathVariable("category-uuid") categoryId: UUID): ResponseEntity<Unit> {
+        return ResponseEntity(
+            HttpStatus.valueOf(
+                deActivateCategory.deActivateCategory(categoryId, UUID.randomUUID()) //TODO User Id
+            )
+        )
     }
 
 }
