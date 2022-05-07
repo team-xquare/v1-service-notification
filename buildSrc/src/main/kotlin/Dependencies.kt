82bc5f2ea198d6d083da57743cfe5fe0ea534b61
sung07288346@gmail.com <>
2022-05-07 18:55:09 +0900

    📑 :: Dependencies

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,20 @@
+object Dependencies {
+    const val SPRING_STARTER = "org.springframework.boot:spring-boot-starter-web"
+    const val SPRING_VALIDATION = "org.springframework.boot:spring-boot-starter-validation"
+    const val SPRING_SECURITY = "org.springframework.boot:spring-boot-starter-security"
+
+    const val JACKSON = "com.fasterxml.jackson.module:jackson-module-kotlin"
+
+    const val AWS_MESSAGING = "org.springframework.cloud:spring-cloud-starter-aws-messaging:${DependencyVersions.AWS_MESSAGING}"
+
+    const val QUERYDSL = "com.querydsl:querydsl-jpa:${DependencyVersions.QUERYDSL}"
+    const val QUERYDSL_PROCESSOR = "com.querydsl:querydsl-apt:${DependencyVersions.QUERYDSL}:jpa"
+
+    const val JPA = "org.springframework.boot:spring-boot-starter-data-jpa"
+    const val MYSQL_CONNECTOR = "mysql:mysql-connector-java"
+
+    const val MAPSTRUCT = "org.mapstruct:mapstruct:1.4.2.Final"
+    const val MAPSTRUCT_PROCESSOR = "org.mapstruct:mapstruct-processor:1.4.2.Final"
+
+    const val SPRING_TEST = "org.springframework.boot:spring-boot-starter-test"
+}
