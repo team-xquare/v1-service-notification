4bb94b24a3a0cb8738a6c8ccb9cad3731482cb80
sung07288346@gmail.com <>
2022-05-07 18:56:22 +0900

    📑 :: PuluginVersions

---
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -0,0 +1,6 @@
+object PluginVersions {
+    const val SPRING_VERSION = "2.6.7"
+    const val DEPENDENCY_MANAGEMENT = "1.0.11.RELEASE"
+    const val SPRING_PLUGIN = "1.6.21"
+    const val JPA_PLUGIN = "1.6.10"
+}
