abe116ed6702aebd9f496e88748cbbc7a832c841
leeseojune53 <sung07288346@gmail.com>
2022-05-10 22:09:01 +0900

    🧪 :: QueryNotificationCategoryImplTest

---
+++ notification-domain/src/test/kotlin/io/github/v1servicenotification/category/QueryNotificationCategoryImplTest.kt
@@ -0,0 +1,43 @@
+package io.github.v1servicenotification.category
+
+import io.github.v1servicenotification.category.queryCategory.service.QueryNotificationCategoryImpl
+import io.github.v1servicenotification.stubs.InMemoryCategoryRepository
+import org.junit.jupiter.api.Test
+import java.util.*
+import org.assertj.core.api.Assertions.*
+
+class QueryNotificationCategoryImplTest {
+
+    private val notificationSpi = InMemoryCategoryRepository()
+    private val queryNotificationCategory = QueryNotificationCategoryImpl(notificationSpi)
+
+    @Test
+    fun queryNotificationCategory() {
+        notificationSpi.saveCategory(
+            Category(
+                UUID.randomUUID(),
+                "Test category",
+                "Test destination",
+                true
+            )
+        )
+        assertThat(queryNotificationCategory.queryNotificationCategory().categories.size)
+            .isEqualTo(1)
+    }
+
+    @Test
+    fun queryNotificationCategoryEmpty() {
+        notificationSpi.saveCategory(
+            Category(
+                UUID.randomUUID(),
+                "Test category",
+                "Test destination",
+                false
+            )
+        )
+
+        assertThat(queryNotificationCategory.queryNotificationCategory().categories.size)
+            .isEqualTo(0)
+    }
+
+}
