ebeb0845b9b6ddc65fd129a357cc1a8d762301b3
leeseojune53 <sung07288346@gmail.com>
2022-05-11 22:51:30 +0900

    ⚡️ :: 다른 Spi 의존성 제거

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/stubs/InMemoryDetailRepository.kt
+++ notification-domain/src/main/kotlin/io/github/v1servicenotification/stubs/InMemoryDetailRepository.kt
@@ -1,15 +1,19 @@
 package io.github.v1servicenotification.stubs
 
-import io.github.v1servicenotification.category.queryCategory.spi.CategoryRepositorySpi
+import io.github.v1servicenotification.category.Category
 import io.github.v1servicenotification.detail.Detail
 import io.github.v1servicenotification.detail.queryDetail.spi.DetailRepositorySpi
 import io.github.v1servicenotification.detail.queryDetail.spi.dto.DetailModel
 import java.util.*
 
 class InMemoryDetailRepository(
-    private val categoryRepositorySpi: CategoryRepositorySpi,
+    private val categoryMap: HashMap<UUID, Category> = hashMapOf(),
     private val detailMap: HashMap<UUID, Detail> = hashMapOf()
 ) : DetailRepositorySpi {
+
+    fun saveCategory(category: Category) {
+        categoryMap[category.id] = category
+    }
 
     fun saveDetail(detail: Detail) {
         detailMap[detail.id] = detail
@@ -18,7 +22,8 @@ class InMemoryDetailRepository(
     override fun findAllByUserId(userId: UUID): List<DetailModel> {
         return detailMap.filter { it.value.userId == userId }
             .map {
-                val category = categoryRepositorySpi.findById(it.value.notificationCategoryId)
+                val category = categoryMap[it.value.notificationCategoryId]
+                    ?: throw RuntimeException()
                 DetailModel(
                     it.value.id,
                     it.value.title,
