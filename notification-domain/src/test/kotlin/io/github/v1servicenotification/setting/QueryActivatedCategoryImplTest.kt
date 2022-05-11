0eebe4ab8219d4c61bccdd6943ccc107fb292d99
leeseojune53 <sung07288346@gmail.com>
2022-05-11 19:12:09 +0900

    🧪 :: QueryActivatedCategoryImplTest

---
+++ notification-domain/src/test/kotlin/io/github/v1servicenotification/setting/QueryActivatedCategoryImplTest.kt
@@ -0,0 +1,42 @@
+package io.github.v1servicenotification.setting
+
+import io.github.v1servicenotification.category.Category
+import io.github.v1servicenotification.setting.activeSetting.service.QueryActivatedCategoryImpl
+import io.github.v1servicenotification.stubs.InMemoryCategoryRepository
+import io.github.v1servicenotification.stubs.InMemorySettingRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class QueryActivatedCategoryImplTest {
+
+    private val categorySpi = InMemoryCategoryRepository()
+
+    private val settingSpi = InMemorySettingRepository(categorySpi)
+
+    private val queryActivatedCategory = QueryActivatedCategoryImpl(settingSpi)
+
+    @Test
+    fun queryActivatedCategory() {
+        val userId = UUID.randomUUID()
+        val categoryId = UUID.randomUUID()
+
+        val category = Category(categoryId, "Test name", "Test destination", false)
+
+        categorySpi.saveCategory(category)
+
+        settingSpi.saveSetting(
+            category,
+            userId,
+            true
+        )
+
+        val result = queryActivatedCategory.queryActivatedCategory(userId).categories[0]
+
+        assertThat(result.id).isEqualTo(categoryId)
+        assertThat(result.name).isEqualTo(category.name)
+        assertThat(result.destination).isEqualTo(category.destination)
+
+    }
+
+}
