89ee1f8f3f6e76e6af531d37d8cb6bd86d9a9572
leeseojune53 <sung07288346@gmail.com>
2022-05-11 19:11:03 +0900

    ⚡️ :: 사용하지않는 field

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/setting/activeSetting/service/QueryActivatedCategoryImpl.kt
+++ notification-domain/src/main/kotlin/io/github/v1servicenotification/setting/activeSetting/service/QueryActivatedCategoryImpl.kt
@@ -3,14 +3,12 @@ package io.github.v1servicenotification.setting.activeSetting.service
 import io.github.v1servicenotification.annotation.DomainService
 import io.github.v1servicenotification.category.queryCategory.api.dto.response.CategoryElement
 import io.github.v1servicenotification.category.queryCategory.api.dto.response.CategoryListResponse
-import io.github.v1servicenotification.category.queryCategory.spi.CategoryRepositorySpi
 import io.github.v1servicenotification.setting.activeSetting.api.QueryActivatedCategory
 import io.github.v1servicenotification.setting.activeSetting.spi.SettingRepositorySpi
 import java.util.*
 
 @DomainService
 class QueryActivatedCategoryImpl(
-    private val categoryRepositorySpi: CategoryRepositorySpi,
     private val settingRepositorySpi: SettingRepositorySpi
 ) : QueryActivatedCategory {
     override fun queryActivatedCategory(userId: UUID): CategoryListResponse {
