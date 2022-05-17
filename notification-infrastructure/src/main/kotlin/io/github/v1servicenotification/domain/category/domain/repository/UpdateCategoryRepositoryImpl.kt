41e899bc6f9082b059156d98b8d72cd1f84d74c0
leeseojune53 <sung07288346@gmail.com>
2022-05-18 00:03:46 +0900

    📑 :: UpdateCategoryRepositoryImpl

---
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/category/domain/repository/UpdateCategoryRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package io.github.v1servicenotification.domain.category.domain.repository
+
+import io.github.v1servicenotification.category.Category
+import io.github.v1servicenotification.category.updateCategory.spi.UpdateCategoryRepositorySpi
+import io.github.v1servicenotification.domain.category.mapper.CategoryMapper
+import org.springframework.stereotype.Repository
+
+@Repository
+class UpdateCategoryRepositoryImpl(
+    private val categoryMapper: CategoryMapper,
+    private val categoryRepository: CategoryRepository
+): UpdateCategoryRepositorySpi {
+
+    override fun saveCategory(category: Category): Category {
+        val categoryEntity = categoryMapper.categoryDomainToEntity(category)
+        return categoryMapper.categoryEntityToDomain(
+            categoryRepository.save(categoryEntity)
+        )
+    }
+
+
+}
