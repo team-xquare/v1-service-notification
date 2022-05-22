4d6407711b7821c89bf75d5c1ce31e562a2a22db
Leeseojune <sung07288346@gmail.com>
2022-05-22 19:54:26 +0900

    🔀 :: (#72) Mapper UnMapped properties

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/mapper/DetailMapper.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/mapper/DetailMapper.kt
@@ -3,9 +3,16 @@ package io.github.v1servicenotification.domain.detail.mapper
 import io.github.v1servicenotification.detail.Detail
 import io.github.v1servicenotification.domain.detail.domain.DetailEntity
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.Mappings
 
 @Mapper
 interface DetailMapper {
     fun detailDomainToEntity(detail: Detail): DetailEntity
+
+    @Mappings(
+        Mapping(source = "categoryEntity.id", target = "notificationCategoryId"),
+        Mapping(source = "read", target = "isRead")
+    )
     fun detailEntityToDomain(detailEntity: DetailEntity): Detail
 }
--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/setting/mapper/SettingMapper.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/setting/mapper/SettingMapper.kt
@@ -1,11 +1,23 @@
 package io.github.v1servicenotification.domain.setting.mapper
 
+import io.github.v1servicenotification.domain.category.domain.CategoryEntity
 import io.github.v1servicenotification.domain.setting.domain.SettingEntity
+import io.github.v1servicenotification.domain.setting.domain.SettingId
 import io.github.v1servicenotification.setting.Setting
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.Mappings
 
 @Mapper
 interface SettingMapper {
+
+
     fun settingDomainToEntity(setting: Setting): SettingEntity
+
+    @Mappings(
+        Mapping(source = "settingId.userId", target = "userId"),
+        Mapping(source = "settingId.categoryEntity.id", target = "notificationCategoryId"),
+        Mapping(source = "activated", target = "isActivated")
+    )
     fun settingEntityToDomain(settingEntity: SettingEntity): Setting
 }
