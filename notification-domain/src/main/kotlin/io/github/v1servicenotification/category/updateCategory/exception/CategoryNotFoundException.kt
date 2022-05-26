d2f3b9fff8f01676ff9386c90173379e5a706d5d
leeseojune53 <sung07288346@gmail.com>
2022-05-26 13:02:19 +0900

    ⚡️ :: JvmField in Companion object

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/category/updateCategory/exception/CategoryNotFoundException.kt
+++ notification-domain/src/main/kotlin/io/github/v1servicenotification/category/updateCategory/exception/CategoryNotFoundException.kt
@@ -5,6 +5,7 @@ import io.github.v1servicenotification.error.NotificationException
 
 class CategoryNotFoundException private constructor(): NotificationException(ErrorCode.CATEGORY_NOT_FOUND) {
     companion object {
+        @JvmField
         val EXCEPTION = CategoryNotFoundException()
     }
 }
