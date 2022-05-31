b8ad53bec0ecbc8495848d5e36cbb71299c6bd8a
leeseojune53 <sung07288346@gmail.com>
2022-05-31 22:26:59 +0900

    ⚡️ :: QueueController

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/notification/presentation/QueueController.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/notification/presentation/QueueController.kt
@@ -1,19 +1,20 @@
 package io.github.v1servicenotification.domain.notification.presentation
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.convertValue
-import com.fasterxml.jackson.module.kotlin.readValue
 import io.github.v1servicenotification.detail.postDetail.api.PostGroupNotification
 import io.github.v1servicenotification.detail.postDetail.api.PostNotification
-import io.github.v1servicenotification.domain.notification.exception.EmptyArgumentException
 import io.github.v1servicenotification.domain.notification.presentation.dto.Group
 import io.github.v1servicenotification.domain.notification.presentation.dto.Personal
-import org.springframework.cloud.aws.messaging.config.annotation.NotificationMessage
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener
+import org.springframework.messaging.converter.MessageConversionException
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
 import java.util.*
+import javax.validation.Valid
+
 
 @Component
 class QueueController(
@@ -22,21 +23,13 @@ class QueueController(
 ) {
 
     @SqsListener(value = ["group-notification.fifo"], deletionPolicy = SqsMessageDeletionPolicy.ALWAYS)
-    fun groupNotification(@Payload group: Group) {
-        if(group.categoryId == null || group.title == null || group.content == null) {
-            throw EmptyArgumentException.EXCEPTION
-        }
-
-        postGroupNotification.postGroupNotification(group.categoryId, group.title, group.content)
+    fun groupNotification(@Payload @Valid group: Group) {
+        postGroupNotification.postGroupNotification(group.categoryId!!, group.title!!, group.content!!)
     }
 
     @SqsListener(value = ["notification.fifo"], deletionPolicy = SqsMessageDeletionPolicy.ALWAYS)
-    fun notification(@Payload personal: Personal) {
-        if(personal.userId == null || personal.categoryId == null || personal.title == null || personal.content == null) {
-            throw EmptyArgumentException.EXCEPTION
-        }
-
-        postNotification.postNotification(personal.userId, personal.categoryId, personal.title, personal.content)
+    fun notification(@Payload @Valid personal: Personal) {
+        postNotification.postNotification(personal.userId!!, personal.categoryId!!, personal.title!!, personal.content!!)
     }
 
 }
