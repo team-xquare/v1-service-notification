fcf050aa538ebacad570b8dba96f7980c6abf000
leeseojune53 <sung07288346@gmail.com>
2022-05-31 22:25:57 +0900

    📑 :: CustomPayloadMethodArgumentResolver

---
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/global/config/CustomPayloadMethodArgumentResolver.kt
@@ -0,0 +1,32 @@
+package io.github.v1servicenotification.global.config
+
+import org.springframework.core.MethodParameter
+import org.springframework.messaging.Message
+import org.springframework.messaging.converter.MessageConverter
+import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver
+import org.springframework.validation.Validator
+import org.springframework.web.bind.MethodArgumentNotValidException
+
+class CustomPayloadMethodArgumentResolver(
+    converter: MessageConverter,
+    validator: Validator
+) : HandlerMethodArgumentResolver {
+    private val payloadMethodArgumentResolver =
+        PayloadMethodArgumentResolver(converter, validator)
+
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return payloadMethodArgumentResolver.supportsParameter(parameter)
+    }
+
+    override fun resolveArgument(parameter: MethodParameter, message: Message<*>): Any? {
+        try {
+            payloadMethodArgumentResolver.resolveArgument(parameter, message)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return payloadMethodArgumentResolver.resolveArgument(parameter, message)
+    }
+
+}
