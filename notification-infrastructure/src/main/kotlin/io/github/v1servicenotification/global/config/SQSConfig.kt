ac67c2458da2ec941807d4f37f0cd65268c2887b
leeseojune53 <sung07288346@gmail.com>
2022-05-31 22:26:46 +0900

    ⚡️ :: CustomPayload 적용

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/global/config/SQSConfig.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/global/config/SQSConfig.kt
@@ -1,13 +1,20 @@
 package io.github.v1servicenotification.global.config
 
+import com.amazonaws.services.sqs.AmazonSQSAsync
 import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory
+import org.springframework.cloud.aws.messaging.listener.QueueMessageHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.converter.MappingJackson2MessageConverter
+import org.springframework.messaging.converter.MessageConverter
 import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver
+import org.springframework.validation.Validator
+
 
 @Configuration
-class SQSConfig {
+class SQSConfig(
+    private val validator: Validator
+) {
 
     @Bean
     fun queueMessageHandlerFactory(): QueueMessageHandlerFactory? {
@@ -17,7 +24,7 @@ class SQSConfig {
         messageConverter.isStrictContentTypeMatch = false
 
         factory.setArgumentResolvers(
-            listOf(PayloadMethodArgumentResolver(messageConverter))
+            listOf(CustomPayloadMethodArgumentResolver(messageConverter, validator))
         )
 
         return factory
