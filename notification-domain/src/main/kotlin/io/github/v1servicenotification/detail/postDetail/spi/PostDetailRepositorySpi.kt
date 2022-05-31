1b113d4c99567135ad9f6ddea5b2285f017ab6b9
leeseojune53 <sung07288346@gmail.com>
2022-05-31 09:41:52 +0900

    ⚡️ :: saveDetail

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/detail/postDetail/spi/PostDetailRepositorySpi.kt
+++ notification-domain/src/main/kotlin/io/github/v1servicenotification/detail/postDetail/spi/PostDetailRepositorySpi.kt
@@ -3,5 +3,6 @@ package io.github.v1servicenotification.detail.postDetail.spi
 import io.github.v1servicenotification.detail.Detail
 
 interface PostDetailRepositorySpi {
+    fun save(detail: Detail)
     fun saveAllDetail(detailList: List<Detail>)
 }
