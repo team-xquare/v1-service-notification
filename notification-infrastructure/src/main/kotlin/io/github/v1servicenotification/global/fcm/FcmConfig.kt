f49ce06fcaf60924a562a24ad2a9e79fb1079112
leeseojune53 <sung07288346@gmail.com>
2022-06-08 14:53:59 +0900

    ⚡️ :: FcmConfig

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/global/fcm/FcmConfig.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/global/fcm/FcmConfig.kt
@@ -5,14 +5,15 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.ClassPathResource
+import java.io.ByteArrayInputStream
 import java.io.IOException
 import javax.annotation.PostConstruct
 
+
 @Configuration
 class FcmConfig(
-    @Value("\${fcm.path}")
-    private val path: String
+    @Value("\${fcm.value}")
+    private val value: String
 ) {
 
     @PostConstruct
@@ -20,7 +21,7 @@ class FcmConfig(
         try {
             if(FirebaseApp.getApps().isEmpty()) {
                 val options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(ClassPathResource(path).inputStream))
+                    .setCredentials(GoogleCredentials.fromStream(ByteArrayInputStream(value.toByteArray())))
                     .build()
                 FirebaseApp.initializeApp(options)
             }
