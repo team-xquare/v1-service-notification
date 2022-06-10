56594c8611561e64a6584b15bbe04842e2012dc2
Leeseojune <sung07288346@gmail.com>
2022-06-10 16:23:26 +0900

    🔀 :: (#101) Detail Mapper 구현

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/mapper/DetailMapper.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/mapper/DetailMapper.kt
@@ -2,17 +2,9 @@ package io.github.v1servicenotification.domain.detail.mapper
 
 import io.github.v1servicenotification.detail.Detail
 import io.github.v1servicenotification.domain.detail.domain.DetailEntity
-import org.mapstruct.Mapper
-import org.mapstruct.Mapping
-import org.mapstruct.Mappings
 
-@Mapper
 interface DetailMapper {
     fun detailDomainToEntity(detail: Detail): DetailEntity
 
-    @Mappings(
-        Mapping(source = "categoryEntity.id", target = "categoryId"),
-        Mapping(source = "read", target = "isRead")
-    )
     fun detailEntityToDomain(detailEntity: DetailEntity): Detail
 }
---
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/mapper/DetailMapperImpl.kt
@@ -0,0 +1,44 @@
+package io.github.v1servicenotification.domain.detail.mapper
+
+import io.github.v1servicenotification.detail.Detail
+import io.github.v1servicenotification.domain.category.domain.CategoryEntity
+import io.github.v1servicenotification.domain.category.domain.repository.CategoryRepository
+import io.github.v1servicenotification.domain.category.domain.repository.CustomCategoryRepositoryImpl
+import io.github.v1servicenotification.domain.category.exception.CategoryNotFoundException
+import io.github.v1servicenotification.domain.detail.domain.DetailEntity
+import org.springframework.stereotype.Component
+import java.util.UUID
+
+@Component
+class DetailMapperImpl(
+     private val categoryRepository: CategoryRepository
+) : DetailMapper {
+    override fun detailDomainToEntity(detail: Detail): DetailEntity {
+        val categoryEntity = categoryRepository.findById(detail.categoryId)
+            .orElseThrow {
+                CategoryNotFoundException.EXCEPTION
+            }
+
+        return DetailEntity(
+            id = detail.id,
+            title = detail.title,
+            content = detail.content,
+            sentAt = detail.sentAt,
+            isRead = detail.isRead,
+            userId = detail.userId,
+            categoryEntity = categoryEntity
+        )
+    }
+
+    override fun detailEntityToDomain(detailEntity: DetailEntity): Detail {
+        return Detail(
+            id = detailEntity.id!!,
+            title = detailEntity.title,
+            content = detailEntity.content,
+            sentAt = detailEntity.sentAt!!,
+            isRead = detailEntity.isRead,
+            userId = detailEntity.userId,
+            categoryId = detailEntity.getCategoryId()
+        )
+    }
+}
