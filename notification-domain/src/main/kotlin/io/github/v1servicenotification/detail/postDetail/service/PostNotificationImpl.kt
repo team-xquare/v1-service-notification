18f0e51a229fc0d68f80dfd6a5c9f42a53325dd9
leeseojune53 <sung07288346@gmail.com>
2022-06-18 05:26:45 +0900

    ⚡️ :: User Spi 적용

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/detail/postDetail/service/PostNotificationImpl.kt
+++ notification-domain/src/main/kotlin/io/github/v1servicenotification/detail/postDetail/service/PostNotificationImpl.kt
@@ -1,22 +1,28 @@
 package io.github.v1servicenotification.detail.postDetail.service
 
 import io.github.v1servicenotification.annotation.DomainService
+import io.github.v1servicenotification.category.queryCategory.spi.QueryCategoryRepositorySpi
+import io.github.v1servicenotification.category.updateCategory.exception.CategoryNotFoundException
 import io.github.v1servicenotification.detail.Detail
 import io.github.v1servicenotification.detail.postDetail.api.PostNotification
 import io.github.v1servicenotification.detail.postDetail.spi.PostDetailFcmSpi
 import io.github.v1servicenotification.detail.postDetail.spi.PostDetailRepositorySpi
+import io.github.v1servicenotification.detail.postDetail.spi.PostDetailUserSpi
 import java.time.LocalDateTime
 import java.util.*
 
 @DomainService
 class PostNotificationImpl(
     private val postDetailRepositorySpi: PostDetailRepositorySpi,
-    private val postDetailFcmSpi: PostDetailFcmSpi
+    private val postDetailFcmSpi: PostDetailFcmSpi,
+    private val queryCategoryRepositorySpi: QueryCategoryRepositorySpi,
+    private val postDetailUserSpi: PostDetailUserSpi
 ): PostNotification {
 
     override fun postNotification(userId: UUID, categoryId: UUID, title: String, content: String) {
-        //TODO User service에 token 받아오는 spi
-
+        if(!queryCategoryRepositorySpi.exist(categoryId)) {
+            throw CategoryNotFoundException.EXCEPTION
+        }
 
         postDetailRepositorySpi.save(
             Detail(
@@ -29,6 +35,6 @@ class PostNotificationImpl(
             )
         )
 
-        postDetailFcmSpi.sendMessage("", title, content)
+        postDetailFcmSpi.sendMessage(postDetailUserSpi.getDeviceToken(userId), title, content)
     }
 }
