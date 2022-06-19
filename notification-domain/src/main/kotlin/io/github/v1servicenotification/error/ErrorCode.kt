673a710ba15bee88f7165cb5f551c5c5f017943f
leeseojune53 <sung07288346@gmail.com>
2022-06-19 17:11:55 +0900

    ⚡️ :: ErrorCode

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/error/ErrorCode.kt
+++ notification-domain/src/main/kotlin/io/github/v1servicenotification/error/ErrorCode.kt
@@ -5,6 +5,15 @@ enum class ErrorCode(
     val code: String,
     val message: String
 ) {
+    OTHER_BAD_REQUEST(400, "FEIGN-400-1", "Other Bad Request."),
+
+    OTHER_UNAUTHORIZED(401, "FEIGN-401-1", "Other Unauthorized."),
+
+    OTHER_FORBIDDEN(403, "FEIGN-403-1", "Other Forbidden."),
+
+    OTHER_EXPIRED_TOKEN(419, "FEIGN-419-1", "Other Expired Token."),
+
+
     EMPTY_ARGUMENT(400, "NOTIFICATION-400-1", "Empty argument."),
 
     CATEGORY_NOT_FOUND(404, "CATEGORY-404-1", "Category Not Found."),
