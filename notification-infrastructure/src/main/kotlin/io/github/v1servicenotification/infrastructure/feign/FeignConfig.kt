c93f9b63836f56ab165b9b823497337552fca520
leeseojune53 <sung07288346@gmail.com>
2022-06-19 17:12:46 +0900

    ⚙️ :: Feign Config

---
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/infrastructure/feign/FeignConfig.kt
@@ -0,0 +1,19 @@
+package io.github.v1servicenotification.infrastructure.feign
+
+import feign.codec.ErrorDecoder
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@EnableFeignClients(basePackages = ["io.github.v1servicenotification.infrastructure.feign"])
+@Configuration
+class FeignConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(value = [ErrorDecoder::class])
+    fun commonFeignErrorDecoder(): FeignClientErrorDecoder {
+        return FeignClientErrorDecoder()
+    }
+
+}
