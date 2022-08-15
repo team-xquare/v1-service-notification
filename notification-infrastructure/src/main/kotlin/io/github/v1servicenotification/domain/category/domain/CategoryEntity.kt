1b7488bdb6dac3d4af7b7f48404fc134f70698a2
leeseojune53 <sung07288346@gmail.com>
2022-08-15 22:22:43 +0900

    ⚡️ :: Category Setting OneToMany

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/category/domain/CategoryEntity.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/category/domain/CategoryEntity.kt
@@ -1,9 +1,11 @@
 package io.github.v1servicenotification.domain.category.domain
 
+import io.github.v1servicenotification.domain.setting.domain.SettingEntity
 import io.github.v1servicenotification.global.entity.BaseUUIDEntity
 import java.util.UUID
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.OneToMany
 import javax.persistence.Table
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
@@ -22,5 +24,8 @@ class CategoryEntity(
 
     @field:NotNull
     @Column(columnDefinition = "TINYINT(1) DEFAULT 0")
-    val defaultActivated: Boolean
+    val defaultActivated: Boolean,
+
+    @OneToMany(mappedBy = "settingId.categoryEntity")
+    val settingList: Set<SettingEntity> = HashSet()
 ) : BaseUUIDEntity(id)
