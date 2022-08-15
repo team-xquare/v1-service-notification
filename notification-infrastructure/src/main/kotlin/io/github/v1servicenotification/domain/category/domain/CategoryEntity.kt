edf4bdf0afb7416c8aeb49d6821cad957e4fa62c
leeseojune53 <sung07288346@gmail.com>
2022-08-15 22:32:26 +0900

    ⚡️ :: Lazy 명시

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/category/domain/CategoryEntity.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/category/domain/CategoryEntity.kt
@@ -3,10 +3,7 @@ package io.github.v1servicenotification.domain.category.domain
 import io.github.v1servicenotification.domain.setting.domain.SettingEntity
 import io.github.v1servicenotification.global.entity.BaseUUIDEntity
 import java.util.UUID
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.OneToMany
-import javax.persistence.Table
+import javax.persistence.*
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
@@ -26,6 +23,6 @@ class CategoryEntity(
     @Column(columnDefinition = "TINYINT(1) DEFAULT 0")
     val defaultActivated: Boolean,
 
-    @OneToMany(mappedBy = "settingId.categoryEntity")
+    @OneToMany(mappedBy = "settingId.categoryEntity", fetch = FetchType.LAZY)
     val settingList: Set<SettingEntity> = HashSet()
 ) : BaseUUIDEntity(id)
