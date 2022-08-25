4c30b5636bb688c32738a870a2bba6e200d9474d
leeseojune53 <sung07288346@gmail.com>
2022-08-25 23:51:10 +0900

    ⚰️ :: 사용하지 않는 파일

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/setting/activeSetting/api/ActivateCategory.kt
+++
@@ -1,7 +0,0 @@
-package io.github.v1servicenotification.setting.activeSetting.api
-
-import java.util.*
-
-interface ActivateCategory {
-    fun activateCategory(categoryId: UUID, userId: UUID): Int
-}
--- notification-domain/src/main/kotlin/io/github/v1servicenotification/setting/activeSetting/api/QueryActivatedCategory.kt
+++
@@ -1,8 +0,0 @@
-package io.github.v1servicenotification.setting.activeSetting.api
-
-import io.github.v1servicenotification.category.api.response.CategoryListResponse
-import java.util.*
-
-interface QueryActivatedCategory {
-    fun queryActivatedCategory(userId: UUID): CategoryListResponse
-}
