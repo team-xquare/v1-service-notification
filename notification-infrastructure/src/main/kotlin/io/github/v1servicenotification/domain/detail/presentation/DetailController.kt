7ce95a4145ae24c9a510ca7a9a0b962c68c66a85
leeseojune53 <sung07288346@gmail.com>
2022-09-13 22:43:48 +0900

    ⚡️ :: Operation, Tag

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/presentation/DetailController.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/presentation/DetailController.kt
@@ -3,22 +3,27 @@ package io.github.v1servicenotification.domain.detail.presentation
 import io.github.v1servicenotification.detail.api.DetailApi
 import io.github.v1servicenotification.detail.api.dto.response.DetailResponse
 import io.github.v1servicenotification.global.extension.getUserId
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
+@Tag(name = "알림 상세", description = "알림 상세 Api입니다.")
 @RestController
 class DetailController(
     private val detailApi: DetailApi
 ) {
 
+    @Operation(summary = "전송된 알림 목록")
     @GetMapping("/")
     fun queryNotificationDetailList(): DetailResponse {
         return detailApi.queryNotificationDetail(getUserId())
     }
 
+    @Operation(summary = "전송된 알림 목록")
     @PostMapping("/{notification-uuid}")
     fun checkNotification(@PathVariable("notification-uuid") notificationId: UUID) {
         detailApi.checkNotification(getUserId(), notificationId)
--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/setting/presentation/SettingController.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/setting/presentation/SettingController.kt
@@ -3,23 +3,28 @@ package io.github.v1servicenotification.domain.setting.presentation
 import io.github.v1servicenotification.category.api.response.CategoryListResponse
 import io.github.v1servicenotification.global.extension.getUserId
 import io.github.v1servicenotification.setting.api.SettingApi
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
+@Tag(name = "알림 카테고리 활성화 설정", description = "알림 카테고리 활성화 관련 Api 입니다.")
 @RequestMapping("/tags")
 @RestController
 class SettingController(
     private val settingApi: SettingApi
 ) {
 
+    @Operation(summary = "활성화된 알림 카테고리 목록")
     @GetMapping
     fun queryActivatedCategory(): CategoryListResponse {
         return settingApi.queryActivatedCategory(getUserId())
     }
 
+    @Operation(summary = "알림 카테고리 활성화")
     @PatchMapping("/{category-uuid}")
     fun activateNotificationCategory(@PathVariable("category-uuid") categoryId: UUID): ResponseEntity<Unit> {
         return ResponseEntity(
@@ -29,6 +34,7 @@ class SettingController(
         )
     }
 
+    @Operation(summary = "알림 카테고리 비활성회")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{category-uuid}")
     fun deActivateNotificationCategory(@PathVariable("category-uuid") categoryId: UUID): ResponseEntity<Unit> {
