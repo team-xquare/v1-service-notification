e981c5d83217f1413bcedaf9d06b0305c75f0ccd
lyutvs <lyutvs@gmail.com>
2023-03-20 19:17:00 +0900

    🔀 :: mapper 500에러 해결

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/category/mapper/CategoryMapper.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/category/mapper/CategoryMapper.kt
@@ -4,7 +4,6 @@ import io.github.v1servicenotification.category.Category
 import io.github.v1servicenotification.domain.category.domain.CategoryEntity
 import org.mapstruct.Mapper
 
-@Mapper
 interface CategoryMapper {
     fun categoryDomainToEntity(category: Category): CategoryEntity
     fun categoryEntityToDomain(categoryEntity: CategoryEntity): Category
---
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/category/mapper/CategoryMapperImpl.kt
@@ -0,0 +1,29 @@
+package io.github.v1servicenotification.domain.category.mapper
+
+import io.github.v1servicenotification.category.Category
+import io.github.v1servicenotification.domain.category.domain.CategoryEntity
+import org.springframework.stereotype.Component
+
+@Component
+class CategoryMapperImpl: CategoryMapper {
+
+    override fun categoryDomainToEntity(category: Category): CategoryEntity {
+        return CategoryEntity(
+            id = category.id,
+            name = category.name,
+            destination = category.destination,
+            defaultActivated = category.defaultActivated,
+            categoryImageUrl = category.categoryImageUrl
+        )
+    }
+
+    override fun categoryEntityToDomain(categoryEntity: CategoryEntity): Category {
+        return Category(
+            id = categoryEntity.id,
+            name = categoryEntity.name,
+            destination = categoryEntity.destination,
+            defaultActivated = categoryEntity.defaultActivated,
+            categoryImageUrl = categoryEntity.categoryImageUrl
+        )
+    }
+}
