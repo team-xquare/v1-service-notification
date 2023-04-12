e5ea3c2b93f0f26a5b999eeff35834d0f814668f
lyutvs <lyutvs@gmail.com>
2023-04-12 11:48:09 +0900

    ⚡️ :: 테스트 알림 전송

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/infrastructure/feign/client/UserClient.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/infrastructure/feign/client/UserClient.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import java.util.UUID
 
-@FeignClient(name = "UserClient", url = "\${user.url}")
+@FeignClient(name = "UserClient", url = "\${service.scheme}://\${service.user.host}")
 interface UserClient {
 
     @GetMapping("/device-token")
