f9795854775d03a6793c9994f7b98c7b2b72d1c8
lyutvs <lyutvs@gmail.com>
2023-04-21 11:18:07 +0900

    ♻️ :: 파일명 변경 및 필요없는 파일 삭제

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/detail/api/NotificationDetailApi.kt
+++ notification-domain/src/main/kotlin/io/github/v1servicenotification/detail/api/NotificationDetailApi.kt
@@ -4,9 +4,9 @@ import io.github.v1servicenotification.detail.api.dto.response.DetailResponse
 import java.util.UUID
 import io.github.v1servicenotification.detail.api.dto.response.NotificationCountResponse
 
-interface DetailApi {
-    fun postGroupNotification(categoryId: UUID, title: String, content: String)
-    fun postNotification(userId: UUID, categoryId: UUID, title: String, content: String)
+interface NotificationDetailApi {
+    fun postGroupNotification(topic: String, content: String)
+    fun postNotification(userId: UUID, topic: String, content: String)
     fun queryNotificationDetail(userId: UUID): DetailResponse
     fun queryUnreadNotificationCount(userId: UUID): NotificationCountResponse
 }
