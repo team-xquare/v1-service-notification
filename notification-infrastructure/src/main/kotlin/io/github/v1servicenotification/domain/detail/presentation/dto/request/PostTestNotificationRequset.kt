e71ce28bc8c363422c7d6df8770f163e89658988
lyutvs <lyutvs@gmail.com>
2023-05-04 09:36:10 +0900

    🐛 :: 500 에러 해결

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/presentation/dto/request/PostTestNotificationRequset.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/detail/presentation/dto/request/PostTestNotificationRequset.kt
@@ -1,19 +1,19 @@
 package io.github.v1servicenotification.domain.detail.presentation.dto.request
 
 import java.util.UUID
-import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class PostTestNotificationRequset(
 
-    @field:NotBlank
+    @field:NotNull
     val userId: UUID,
 
-    @field:NotBlank
+    @field:NotNull
     val topic: String,
 
-    @field:NotBlank
+    @field:NotNull
     val content: String,
 
-    @field:NotBlank
+    @field:NotNull
     val threadId: String,
 )
