3b921a3ae13305b5a795904684ea3848959ea57d
lyutvs <lyutvs@gmail.com>
2023-05-12 02:01:30 +0900

    ♻️ :: 코드리뷰

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/error/NotificationDeviceTokenLengthException.kt
+++
@@ -1,9 +0,0 @@
-package io.github.v1servicenotification.error
-
-class NotificationDeviceTokenLengthException private constructor(): NotificationException(ErrorCode.DEVICE_TOKEN_LENGTH) {
-
-    companion object {
-        @JvmField
-        val EXCEPTION = NotificationDeviceTokenLengthException()
-    }
-}
