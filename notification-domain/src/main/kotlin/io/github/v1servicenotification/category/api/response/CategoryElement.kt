1b106dc944de5c5882310b076cb63682f5cc7eaf
lyutvs <lyutvs@gmail.com>
2023-05-21 20:24:21 +0900

    ⚡️ :: Response변경

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/category/api/response/CategoryElement.kt
+++ notification-domain/src/main/kotlin/io/github/v1servicenotification/category/api/response/CategoryElement.kt
@@ -1,10 +1,6 @@
 package io.github.v1servicenotification.category.api.response
 
-import java.util.UUID
-
 class CategoryElement(
-    val id: UUID,
-    val title: String,
-    val destination: String,
     val topic: String,
+    val isActivate: Boolean,
 )
