7c76fdc655a6f2c2f502caa995ba742b7d7ed12f
lyutvs <lyutvs@gmail.com>
2023-05-22 20:02:02 +0900

    ♻️ :: 전으로 변경

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/category/api/response/CategoryElement.kt
+++ notification-domain/src/main/kotlin/io/github/v1servicenotification/category/api/response/CategoryElement.kt
@@ -1,6 +1,10 @@
 package io.github.v1servicenotification.category.api.response
 
+import java.util.UUID
+
 class CategoryElement(
+    val id: UUID,
+    val title: String,
+    val destination: String,
     val topic: String,
-    val isActivate: Boolean,
 )
