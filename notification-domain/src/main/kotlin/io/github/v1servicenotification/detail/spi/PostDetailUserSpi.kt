28012d799dff11999b6f85ec3f903f54ec9e465e
주영 <jyk102999@dsm.hs.kr>
2023-06-01 23:22:36 +0900

    ♻️ :: 리턴타입 nullable 설정

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/detail/spi/PostDetailUserSpi.kt
+++ notification-domain/src/main/kotlin/io/github/v1servicenotification/detail/spi/PostDetailUserSpi.kt
@@ -3,7 +3,7 @@ package io.github.v1servicenotification.detail.spi
 import java.util.UUID
 
 interface PostDetailUserSpi {
-    fun getExcludeUserIdList(userIdList: List<UUID>?): List<UUID>
+    fun getExcludeUserIdList(userIdList: List<UUID>?): List<UUID>?
     fun getDeviceToken(userId: UUID): String
     fun getDeviceTokenList(userIdList: List<UUID>): List<String>
 }
