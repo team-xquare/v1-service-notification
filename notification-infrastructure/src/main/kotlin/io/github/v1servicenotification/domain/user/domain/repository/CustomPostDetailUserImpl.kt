7eefec697dc4a69d2f0a7bd8cf6cee9baddd3432
주영 <jyk102999@dsm.hs.kr>
2023-06-02 00:53:38 +0900

    ♻️ :: 객체로 보내는 로직 삭제

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/user/domain/repository/CustomPostDetailUserImpl.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/user/domain/repository/CustomPostDetailUserImpl.kt
@@ -2,7 +2,6 @@ package io.github.v1servicenotification.domain.user.domain.repository
 
 import io.github.v1servicenotification.detail.spi.PostDetailUserSpi
 import io.github.v1servicenotification.infrastructure.feign.client.UserClient
-import io.github.v1servicenotification.infrastructure.feign.client.dto.request.ExcludeUserIdsRequest
 import io.github.v1servicenotification.infrastructure.feign.error.FeignBadRequestException
 import org.springframework.stereotype.Component
 import java.util.UUID
@@ -12,8 +11,7 @@ class CustomPostDetailUserImpl(
     private val userClient: UserClient
 ) : PostDetailUserSpi {
     override fun getExcludeUserIdList(userIdList: List<UUID>): List<UUID> {
-        val userIdsRequest = ExcludeUserIdsRequest(userIdList)
-        return userClient.getExcludeUserIdList(userIdsRequest).userIdList
+        return userClient.getExcludeUserIdList(userIdList).userIdList
     }
 
     override fun getDeviceToken(userId: UUID): String {
--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/infrastructure/feign/client/dto/request/ExcludeUserIdsRequest.kt
+++
@@ -1,9 +0,0 @@
-package io.github.v1servicenotification.infrastructure.feign.client.dto.request
-
-import java.util.UUID
-import javax.annotation.Nullable
-
-data class ExcludeUserIdsRequest(
-    @field:Nullable
-    val userIds: List<UUID>?,
-)
