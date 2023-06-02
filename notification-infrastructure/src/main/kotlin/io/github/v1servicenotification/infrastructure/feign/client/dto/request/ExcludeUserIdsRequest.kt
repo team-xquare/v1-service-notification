15159fc232e50252654837b1013d9af6f09ae1f7
주영 <jyk102999@dsm.hs.kr>
2023-06-02 14:17:31 +0900

    ♻️ :: 안 쓰는 import 삭제

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/infrastructure/feign/client/dto/request/ExcludeUserIdsRequest.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/infrastructure/feign/client/dto/request/ExcludeUserIdsRequest.kt
@@ -1,7 +1,6 @@
 package io.github.v1servicenotification.infrastructure.feign.client.dto.request
 
 import java.util.UUID
-import javax.annotation.Nullable
 
 data class ExcludeUserIdsRequest(
     val userIdList: List<UUID>,
