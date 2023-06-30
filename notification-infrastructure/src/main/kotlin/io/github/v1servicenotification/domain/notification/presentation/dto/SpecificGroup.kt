ab41914285c53b6a05cb679cbf229ab9e482fe52
주영 <jyk102999@dsm.hs.kr>
2023-06-30 21:45:00 +0900

    ♻️ :: 코드 정렬

--- notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/notification/presentation/dto/SpecificGroup.kt
+++ notification-infrastructure/src/main/kotlin/io/github/v1servicenotification/domain/notification/presentation/dto/SpecificGroup.kt
@@ -3,7 +3,6 @@ package io.github.v1servicenotification.domain.notification.presentation.dto
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.UUID
 
-
 data class SpecificGroup(
     @JsonProperty("user_id_list")
     val userIdList: List<UUID>,
