fd290cae529a5649151bb9344f16b0bc89167176
주영 <jyk102999@dsm.hs.kr>
2023-06-30 16:06:30 +0900

    ♻️ :: 특정 그룹만 알림

--- notification-domain/src/main/kotlin/io/github/v1servicenotification/detail/service/NotificationDetailApiImpl.kt
+++ notification-domain/src/main/kotlin/io/github/v1servicenotification/detail/service/NotificationDetailApiImpl.kt
@@ -61,7 +61,37 @@ class NotificationDetailApiImpl(
             postDetailUserSpi.getDeviceTokenList(userIdList),
             category.title,
             content,
-            threadId,
+            threadId
+        )
+    }
+
+    override fun postSpecificGroupNotification(userIdList: List<UUID>, topic: String, content: String, threadId: String) {
+        if (!queryCategoryRepositorySpi.existByTopic(topic)) {
+            throw CategoryNotFoundException.EXCEPTION
+        }
+
+        val category = queryCategoryRepositorySpi.findByTopic(topic)
+
+        val detailList = userIdList
+            .stream()
+            .map {
+                Detail(
+                    title = category.title,
+                    content = content,
+                    sentAt = LocalDateTime.now(),
+                    isRead = false,
+                    userId = it,
+                    categoryId = category.id,
+                )
+            }.toList()
+
+        postDetailRepositorySpi.saveAllDetail(detailList)
+
+        postDetailFcmSpi.sendGroupMessage(
+            postDetailUserSpi.getDeviceTokenList(userIdList),
+            category.title,
+            content,
+            threadId
         )
     }
 
